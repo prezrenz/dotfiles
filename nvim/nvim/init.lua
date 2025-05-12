0abe2451ba7b5538dbdc8f88a276d0e01f2c335e
prezrenz <73829697+prezrenz@users.noreply.github.com>
2025-05-12 15:51:31 +0800

    bring back noice and setup for webdev
    there might be some problems here

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,7 @@
+require("config.settings")
+require("config.maps")
+require("config.lazy")
+require("config.jdtls")
+require("config.colors")
+
+-- vim.cmd("colorscheme tokyonight-moon") -- for no background terminal
