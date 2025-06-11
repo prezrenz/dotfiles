ef30d8434edf479a942cefe49c52947e495ae536
prezrenz <73829697+prezrenz@users.noreply.github.com>
2025-06-12 01:04:14 +0800

    Update colors.lua
    use chocolatier theme

--- nvim/lua/config/colors.lua
+++ nvim/lua/config/colors.lua
@@ -1,9 +1,9 @@
 -- Color settings
 vim.opt.background = "dark"
 if not vim.fn.has("gui_running") then -- In GUI
-	vim.cmd("colorscheme tokyonight-moon")
+	vim.cmd("colorscheme chocolatier")
 else -- In terminal
-	vim.cmd("colorscheme catppuccin-mocha")
+	vim.cmd("colorscheme chocolatier")
 	vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
 	vim.cmd("hi SignColumn ctermbg=NONE guibg=NONE")
 	vim.cmd("hi ColorColumn ctermbg=NONE guibg=NONE")
