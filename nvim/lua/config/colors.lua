1db65c4a3403523508ebb8f2ab7efe2189b2e297
prezrenz <73829697+prezrenz@users.noreply.github.com>
2025-12-04 02:40:29 +0800

    Update colors.lua
    set NeoSolarized as defaul theme

--- nvim/lua/config/colors.lua
+++ nvim/lua/config/colors.lua
@@ -1,10 +1,12 @@
 -- Color settings
 vim.opt.background = "dark"
-if not vim.fn.has("gui_running") then -- In GUI
-	vim.cmd("colorscheme chocolatier")
-else -- In terminal
-	vim.cmd("colorscheme chocolatier")
-	vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
-	vim.cmd("hi SignColumn ctermbg=NONE guibg=NONE")
-	vim.cmd("hi ColorColumn ctermbg=NONE guibg=NONE")
-end
+vim.cmd("colorscheme NeoSolarized")
+-- if not vim.fn.has("gui_running") then -- In GUI
+-- 	vim.cmd("colorscheme chocolatier")
+-- else -- In terminal
+-- 	vim.cmd("colorscheme chocolatier")
+-- 	vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
+-- 	vim.cmd("hi SignColumn ctermbg=NONE guibg=NONE")
+-- 	vim.cmd("hi ColorColumn ctermbg=NONE guibg=NONE")
+-- end
+
